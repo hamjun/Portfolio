b6fca8f67e198c1f33fd685f92dce3c80d023fe7
hamjun <hamjun7@gmail.com>
2022-10-23 20:42:30 -0600

    Removed Services added Skills

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {BiUser} from 'react-icons/bi'
+import {MdMiscellaneousServices} from 'react-icons/md'
 import {MdOutlineImportContacts} from 'react-icons/md'
 import {FaBook} from 'react-icons/fa'
 import {MdContactSupport} from 'react-icons/md'
@@ -13,8 +14,9 @@ const Nav = () => {
     <nav>
       <a href="/#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><AiOutlineHome /></a>
       <a href="/#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}><BiUser /></a>
+      <a href="/#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active': ''}><MdMiscellaneousServices /></a>
       <a href="/#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}><MdOutlineImportContacts /></a>
-      <a href="/#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#services' ? 'active': ''}><FaBook /></a>
+      <a href="/#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active': ''}><FaBook /></a>
       <a href="/#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active': ''}><MdContactSupport /></a>
     </nav>
   )
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './skills.css'
 import {BsFillCloudCheckFill} from 'react-icons/bs'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
+
 const Skills = () => {
   return (
     <section id='experience'>
@@ -9,54 +11,54 @@ const Skills = () => {
 
       <div className="container experience__container">
         <div className="experience__frontend">
-          <h3>FrontEnd Development</h3>
+          <h3>Languages/Frameworks</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <BsFillCloudCheckFill className='experience__details-icon'/>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>HTML</h4>
+                <h4>Java (Spring Boot)</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillCloudCheckFill className='experience__details-icon'/>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>HTML</h4>
+                <h4>Python</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillCloudCheckFill className='experience__details-icon'/>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>HTML</h4>
+                <h4>ReactJS/NodeJS</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillCloudCheckFill className='experience__details-icon'/>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>HTML</h4>
+                <h4>React-Native</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillCloudCheckFill className='experience__details-icon'/>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>HTML</h4>
+                <h4>HTML/CSS/JS</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillCloudCheckFill className='experience__details-icon'/>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>HTML</h4>
+                <h4>SQL</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
           </div>
         </div>
-        <div className="backend">
-        <h3>BackEnd Development</h3>
+        <div className="experience__backend">
+        <h3>Software</h3>
           <div className="experience__content">
             <article className='experience__details'>
               <BsFillCloudCheckFill className='experience__details-icon'/>
